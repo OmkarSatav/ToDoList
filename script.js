86a2abc7223232ce80b3fc8d01d356e63f075351
OmkarSatav <omkar7v2@gmail.com>
2023-03-12 20:10:26 +0530

    Now notes can be added

--- script.js
+++ script.js
@@ -2,5 +2,7 @@ let addNote = document.getElementByID("addNote");
 
 addNote.addEventListener("click", function () {
     console.log("clicked");
-    document.querySelector(".noteForm").getElementsByClassName.visibility = "visible";
+    document.querySelector(".noteForm").getElementsByClassName.style.display = "block";
 });
+
+console.log("hello World");
