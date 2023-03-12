3579274ccfcc81b292f10407078374fd7a6b0ec6
OmkarSatav <omkar7v2@gmail.com>
2023-03-12 18:48:37 +0530

    Note Form created

---
+++ script.js
@@ -0,0 +1,6 @@
+let addNote = document.getElementByID("addNote");
+
+addNote.addEventListener("click", function () {
+    console.log("clicked");
+    document.querySelector(".noteForm").getElementsByClassName.visibility = "visible";
+});
